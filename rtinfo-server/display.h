8386287c27cbae52aebbce6de06135a64bcaeb79
Daniel Maxime <maxux.unix@gmail.com>
2012-03-07 01:16:05 +0100

    display code update

--- rtinfo-server/display.h
+++ rtinfo-server/display.h
@@ -3,4 +3,8 @@
 	
 	void show_packet(netinfo_packed_t *packed, struct sockaddr_in *remote, client_t *client);
 	void show_packet_network(netinfo_packed_net_t *net, struct sockaddr_in *remote, client_t *client);
+	
+	void refresh_whole();
+	void show_header();
+	void show_net_header();
 #endif
